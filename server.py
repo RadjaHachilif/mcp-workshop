8bee43d815bae4c30ea38d2706224085636f143e
Radja <rhachilif@gmail.com>
2026-06-29 10:18:07 +0200

    Update server.py

--- server.py
+++ server.py
@@ -3,19 +3,11 @@ from mcp.server.fastmcp import FastMCP
 # Create the MCP server
 mcp = FastMCP("demo-server")
 
-
 # Add a simple tool
-@mcp.tool()
-def add(a: int, b: int) -> int:
-    """Add two numbers"""
-    return a + b
-
-
-# Add another tool
 @mcp.tool()
 def hello(name: str) -> str:
     """Say hello"""
-    return f"Hello {name}!"
+    return f"Salam Alaikom {name} :) !"
 
 
 if __name__ == "__main__":
